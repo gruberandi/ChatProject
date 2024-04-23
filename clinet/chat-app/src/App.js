c080ef841d8559774582f9c3d9a0c2a59f9d2877
Andi Gruber <109924492+gruberandi@users.noreply.github.com>
2024-04-23 10:50:26 +0200

    inputs

--- clinet/chat-app/src/App.js
+++ clinet/chat-app/src/App.js
@@ -1,7 +1,26 @@
+import io from 'socket.io-client'
+import { useState } from 'react';
+import Chat from './chat';
+const socket = io.connect("http://localhost:3001")
 function App() {
+  const [userName, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = ()=>{
+    if(userName !== "" && room !== ""){
+      socket.emit("join_room",room)
+    }
+  }
   return (
-    <div></div>
+    <div>
+      <h3>
+      Join Chat
+      </h3>
+      <input type='text' placeholder='John....' onChange={(e)=>setUsername(e.target.value)}/>
+      <input type='text' placeholder='Room ID...' onChange={(e)=>setRoom(e.target.value)}/>
+      <button onClick={joinRoom}>Join A Room</button>
+      <Chat socket = {socket} userName={userName} room={room}/>
+    </div>
   );
 }
-
 export default App;
--- server/index.js
+++ server/index.js
@@ -2,24 +2,29 @@ const express = require('express');
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 
 app.use(cors());
 const server = http.createServer(app);
-const io =  new Server( server , {
-    cors:{
+const io = new Server(server, {
+    cors: {
         origin: "http://localhost:3000",
-        methods:["GET","POST"],
+        methods: ["GET", "POST"],
     }
 });
-io.on("connection",(socket)=>{
-   console.log(socket.id);
-   socket.on("disconnect",()=>{
-      console.log("User disconnected",socket.id)
-   })
+
+io.on("connection", (socket) => {
+    console.log(`User Connected ${socket.id}`);
+    socket.on("Join_room",(data)=>{
+        socket.join(data)
+        console.log(`user with ID: ${socket.id} joined room ${data}`)
+    })
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    });
+
 });
 
-
-server.listen(3001,()=>{
+server.listen(3001, () => {
     console.log("SERVER RUNNING");
 });
